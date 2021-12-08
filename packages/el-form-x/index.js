93d60adacb0064756cd04b7dc82d5e0682a30f29
JackLiR8 <jiakeli1024@gmail.com>
2021-12-08 18:51:12 +0800

    wip(el-form-x): in progress

---
+++ packages/el-form-x/index.js
@@ -0,0 +1,7 @@
+import ElFormX from './src/main'
+
+ElFormX.install = function (Vue) {
+  Vue.component(ElFormX.name, ElFormX)
+}
+
+export default ElFormX
---
+++ packages/el-form-x/src/main.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'ElFormX',
+
+  render () {
+    return <div>ElFormX</div>
+  }
+}
